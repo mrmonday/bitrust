77ec618a9c9a3c30a4b537d14c005b662b491f93
Robert Clipsham <robert@octarineparrot.com>
2014-12-03 20:33:55 +0000

    Add script and index page.

---
+++ cron.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -x
+
+cd $(dirname $0)
+
+git submodule update --remote --merge
+
+cd rust
+
+echo '<pre>' > ../index.html
+git log -n100 --grep '\[breaking-change\]' >> ../index.html
+echo '</pre>' >> ../index.html
+
+cd ..
+
+git commit -am 'Automatic update'
+git push origin gh-pages
