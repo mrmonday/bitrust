505fd6efd81ea6dea1650a3a4864249e46632521
Robert Clipsham <robert@octarineparrot.com>
2014-12-04 14:09:42 +0000

    Pull in a separate script in case the script itself has changed.
    Add date to title.

--- cron.sh
+++ cron.sh
@@ -5,17 +5,4 @@ set -x
 cd $(dirname $0)
 
 git pull origin gh-pages
-
-git submodule update --init --merge
-
-cd rust
-
-echo '<title>BitRust - Breaking Changes in rust-lang/rust</title>' > ../index.html
-echo '<pre>' > ../index.html
-git log -n100 --grep '\[breaking-change\]' >> ../index.html
-echo '</pre>' >> ../index.html
-
-cd ..
-
-git commit --author='BitRust Bot <bitrustbot@octarineparrot.com>'  -am 'Automatic update'
-git push origin gh-pages
+./update.sh
---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+cd $(dirname $0)
+
+git submodule update --init --merge
+
+cd rust
+
+date=$(date -u)
+echo "<title>BitRust - Breaking Changes in rust-lang/rust - updated at $date</title>" > ../index.html
+echo '<pre>' > ../index.html
+git log -n100 --grep '\[breaking-change\]' >> ../index.html
+echo '</pre>' >> ../index.html
+
+cd ..
+
+git commit --author='BitRust Bot <bitrustbot@octarineparrot.com>'  -am 'Automatic update'
+git push origin gh-pages
