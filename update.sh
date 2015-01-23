ea30c2c11408c2fc855c30617f53cb45984345f0
David Campbell <dcampbell24@gmail.com>
2015-01-23 16:34:18 -0500

    Add commit date to the breakage log

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ echo "<!DOCTYPE html><html><head>" > ../index.html
 echo "<title>BitRust - Breaking Changes in rust-lang/rust</title>" >> ../index.html
 echo '</head><body><pre>' >> ../index.html
 
-git log -n100 --grep '\[breaking-change\]' \
+git log -n100 --pretty=fuller --grep '\[breaking-change\]' \
   | sed -e 's/&/\&amp;/g' \
   | sed -e 's/</\&lt;/g' \
         -e 's/>/\&gt;/g' \
